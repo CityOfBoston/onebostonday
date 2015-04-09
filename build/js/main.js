0d813ee8058d8a9cc4a27d4b19e04da4d06b8c9c
Rob Erskine <Rob@RobErskine.com>
2015-04-09 15:28:34 -0400

    adding in mayor video card, adding in footer partial, footer links, base twitter/instagram stream styles

--- build/js/main.js
+++ build/js/main.js
@@ -1,129 +1,132 @@
 $(function(){
 
-	var body = $('body');
-	body.addClass('ready');
+    var body = $('body');
+    body.addClass('ready');
 
-			// BG
-			// TYPE
-			// MARK
-			
-	var colors = [
-		[
-			'#0099FA',
-			'#FFFFFF',
-			'#0B004E'
-		],
-		[
-			'#FF0000',
-			'#FFFFFF',
-			'#0B004E'
-		],
-		[
-			'#00B969',
-			'#FFFFFF',
-			'#0B004E'
-		],
-		[
-			'#000000',
-			'#FFFFFF',
-			'#878787'
-		],
-		[
-			'#E7AE08',
-			'#0B004E',
-			'#FFFFFF'
-		],
-		[
-			'#8A5ADE',
-			'#FFFFFF',
-			'#000000'
-		]
-	];
+            // BG
+            // TYPE
+            // MARK
+            
+    var colors = [
+        [
+            '#0099FA',
+            '#FFFFFF',
+            '#0B004E'
+        ],
+        [
+            '#FF0000',
+            '#FFFFFF',
+            '#0B004E'
+        ],
+        [
+            '#00B969',
+            '#FFFFFF',
+            '#0B004E'
+        ],
+        [
+            '#000000',
+            '#FFFFFF',
+            '#878787'
+        ],
+        [
+            '#E7AE08',
+            '#0B004E',
+            '#FFFFFF'
+        ],
+        [
+            '#8A5ADE',
+            '#FFFFFF',
+            '#000000'
+        ]
+    ];
 
 
-	// DYNAMICALLY CHANGES COLOR VALUES ACROSS THE SITE
-	var currentColor = 1;
-	var setColors = function(){
-		var totalColors = colors.length;
+    // DYNAMICALLY CHANGES COLOR VALUES ACROSS THE SITE
+    var currentColor = 1;
+    var setColors = function(){
+        var totalColors = colors.length;
 
-		if(currentColor === totalColors){
-			currentColor = 1;
-		}
-		else{
-			currentColor++;
-		}
+        if(currentColor === totalColors){
+            currentColor = 1;
+        }
+        else{
+            currentColor++;
+        }
 
-		body.data('bg',colors[(currentColor-1)][0]);
-		body.data('type',colors[(currentColor-1)][1]);	
-		body.data('mark',colors[(currentColor-1)][2]);
-	};
-	//colorChangeID = setInterval(setColors, 6000);
+        body.data('bg',colors[(currentColor-1)][0]);
+        body.data('type',colors[(currentColor-1)][1]);  
+        body.data('mark',colors[(currentColor-1)][2]);
+    };
+    //colorChangeID = setInterval(setColors, 6000);
 
-	// GRABS THOSE COLORS IN FOR THE INTRO
-	var colorChangeIntro = function(){
-		var bg = body.data('bg');
-		var type = body.data('type');
-		var mark = body.data('mark');
+    // GRABS THOSE COLORS IN FOR THE INTRO
+    var colorChangeIntro = function(){
+        var bg = body.data('bg');
+        var type = body.data('type');
+        var mark = body.data('mark');
 
-		$('.main-nav').css('background-color',bg);
-	};
-	colorChangeIntroID = setInterval(colorChangeIntro, 10);
+        $('.main-nav').css('background-color',bg);
+    };
+    colorChangeIntroID = setInterval(colorChangeIntro, 10);
 
-	
-	/*//////////////////////////////////////
+    
+    /*//////////////////////////////////////
     //  video toggle
     //////////////////////////////////////*/
 
-    var video = '<iframe src="https://player.vimeo.com/video/124313553?autoplay=1&title=0&byline=0&portrait=0" width="500" height="213" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    var video = '<iframe src="https://player.vimeo.com/video/124548366?autoplay=1" width="500" height="367" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
     $('.video span').on('click',function(){
-		$('.video span').fadeOut(function(){
-			$('.video img').remove();
-			$('.video').append(video);
-			$('.video').fitVids();
-		});
+        $('.video span').fadeOut(function(){
+            $('.video img').remove();
+            $('.video').append(video);
+            $('.video').fitVids();
+        });
     });
 
-    var middleSlide = (Math.ceil( ($('.slider .activity').length / 2) ) - 1);
 
+    /*//////////////////////////////////////
+    //  community slider
+    //////////////////////////////////////*/
+    var middleSlide = (Math.ceil( ($('.slider .activity').length / 2) ) - 1);
     console.log(middleSlide);
 
-	$('.slider').slick({
-		slide: '.activity',
-		dots: true,
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		adaptiveHeight: true,
-		initialSlide: middleSlide
-	});
+    $('.slider').slick({
+        slide: '.activity',
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        initialSlide: middleSlide
+    });
 
-	/*//////////////////////////////////////
+    /*//////////////////////////////////////
     //  for onebostonday tweet box
     //////////////////////////////////////*/
-	var tweetString = "For #OneBostonDay, ";
-	$('button.tweet').on('click',function(){
-		tweetValue = $('.craft textarea').val();
-		finalTweet = encodeURIComponent(tweetString + tweetValue);
-		tweetIntent = "https://twitter.com/intent/tweet?text="+finalTweet+"&related=marty_walsh,notifyboston";
+    var tweetString = "For #OneBostonDay, ";
+    $('button.tweet').on('click',function(){
+        tweetValue = $('.craft textarea').val();
+        finalTweet = encodeURIComponent(tweetString + tweetValue);
+        tweetIntent = "https://twitter.com/intent/tweet?text="+finalTweet+"&related=marty_walsh,notifyboston";
 
-		popItUp(tweetIntent,300,600);
-	});
+        popItUp(tweetIntent,300,600);
+    });
 
-	var characterCount = 100;
-	$('.craft textarea').on('focus',function(){
-		$(this).on('keyup',function(){
-			var length = $(this).val().length;
-			var charactersLeft = characterCount - length;
-			if(charactersLeft <= 0){
-				$('.craft span').html("Your tweet is too long :( <b>"+charactersLeft+"</b>");
-			}
-			else{
-				$('.craft span').text(charactersLeft);
-			}
-		});
-	});
+    var characterCount = 100;
+    $('.craft textarea').on('focus',function(){
+        $(this).on('keyup',function(){
+            var length = $(this).val().length;
+            var charactersLeft = characterCount - length;
+            if(charactersLeft <= 0){
+                $('.craft span').html("Your tweet is too long :( <b>"+charactersLeft+"</b>");
+            }
+            else{
+                $('.craft span').text(charactersLeft);
+            }
+        });
+    });
 
-	/*//////////////////////////////////////
+    /*//////////////////////////////////////
     //  Burger mobile menu
     //////////////////////////////////////*/
     $('.burger-box').on('click', function(event){
@@ -138,34 +141,58 @@ $(function(){
         }
     });
 
-	/*/////////////////////////
-	// Split up query strings
-	/////////////////////////*/
-	var getQueryVariable = function(variable){
-		var query = window.location.search.substring(1);
-		var vars = query.split("&");
-		for (var i=0;i<vars.length;i++) {
-			var pair = vars[i].split("=");
-			if(pair[0] === variable){
-				return pair[1];
-			}
-		}
+    /*//////////////////////////////////////
+    //  footer links
+    //////////////////////////////////////*/
+    $('.mayor .flickr').on('click',function(){
+        window.open('https://www.flickr.com/people/bosmayorsoffice/', '_blank');
+    });
+    $('.mayor .twitter').on('click',function(){
+        window.open('https://twitter.com/marty_walsh', '_blank');
+    });
+    $('.mayor .facebook').on('click',function(){
+        window.open('https://www.facebook.com/VoteMartyWalsh', '_blank');
+    });
 
-		return(false);
-	};
+    $('.city .instagram').on('click',function(){
+        window.open('https://instagram.com/notifyboston/', '_blank');
+    });
+    $('.city .twitter').on('click',function(){
+        window.open('https://twitter.com/NotifyBoston', '_blank');
+    });
+    $('.city .facebook').on('click',function(){
+        window.open('https://www.facebook.com/cityofboston', '_blank');
+    });
 
-	/*/////////////////////////
-	// open data-target in new tabs
-	/////////////////////////*/
-	var popupWindow = function(){
-		$("a[data-popup]").on("click", function(event){
-			event.preventDefault();
-			window.open($(this)[0].href);
-		});
-	};
-	popupWindow();
 
-	var popItUp = function(url,height,width) {
+    /*/////////////////////////
+    // Split up query strings
+    /////////////////////////*/
+    var getQueryVariable = function(variable){
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] === variable){
+                return pair[1];
+            }
+        }
+
+        return(false);
+    };
+
+    /*/////////////////////////
+    // open data-target in new tabs
+    /////////////////////////*/
+    var popupWindow = function(){
+        $("a[data-popup]").on("click", function(event){
+            event.preventDefault();
+            window.open($(this)[0].href);
+        });
+    };
+    popupWindow();
+
+    var popItUp = function(url,height,width) {
         newwindow=window.open(url,'name','height='+height+',width='+width);
         if (window.focus) {newwindow.focus();}
         return false;
