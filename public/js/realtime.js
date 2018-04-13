5548daaccb856eed78e75830a15053856b9c75ed
Rob Erskine <Rob@RobErskine.com>
2018-04-12 21:19:28 -0400

    updating realtime endpoint

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,20 +1,27 @@
 $(function(){
+
+    var number = 0;
+
     var init = function(){
         $.ajax({
-            url:'http://one-boston-day-wayin-api.hhcctech.com/wayin/count.json',
-            type:'GET',
+            url: 'https://metric.wayin.com/xapi/metric/count?apiKey=103adfe9-a7b9-4824-9916-052f7339d73a&id=campaign:6843:127945::aUT30WCpJSjulXZ4,campaign:6843:127945::aYDVSr2FxLE63wT6',
+            type: 'GET',
+            dataType: 'json',
         })
-        .done(function(data){
-            count = data.results[0].count;
+        .done(function(resp){
+            var twitter = resp.content['campaign:6843:127945::aUT30WCpJSjulXZ4'][0]['count'];
+            var facebook = resp.content['campaign:6843:127945::aYDVSr2FxLE63wT6'][0]['count'];
+            number = twitter + facebook;
+
             var $odometer = document.querySelector('h1');
             od = new Odometer({
                 el: $odometer,
-                value: count,
+                value: number,
                 format: '(,ddd)',
                 duration: 300,
             }); 
 
-            od.update(count);
+            od.update(number);
         });
     };
 
